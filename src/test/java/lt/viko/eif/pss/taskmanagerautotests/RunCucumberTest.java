528c754d11ee6e4fda83a08fc9d110681a4ff5c1
GiedriusMauza <giedrius.mauza@gmail.com>
2023-05-31 12:56:24 +0300

    Initial project template

---
+++ src/test/java/lt/viko/eif/pss/taskmanagerautotests/RunCucumberTest.java
@@ -0,0 +1,15 @@
+package lt.viko.eif.pss.taskmanagerautotests;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("lt/viko/eif/pss/taskmanagerautotests")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+public class RunCucumberTest {
+}
---
+++ src/test/java/lt/viko/eif/pss/taskmanagerautotests/StepDefinitions.java
@@ -0,0 +1,22 @@
+package lt.viko.eif.pss.taskmanagerautotests;
+
+import io.cucumber.java.en.*;
+
+import org.junit.jupiter.api.Assertions.*;
+
+public class StepDefinitions {
+
+    @Given("an example scenario")
+    public void anExampleScenario() {
+        System.out.println("Hello World");
+    }
+
+    @When("all step definitions are implemented")
+    public void allStepDefinitionsAreImplemented() {
+    }
+
+    @Then("the scenario passes")
+    public void theScenarioPasses() {
+    }
+
+}
